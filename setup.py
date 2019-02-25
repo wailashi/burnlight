f40e818982d62355cc1e498933cb8f0d612f3b44
Matthew Casserly <wailashi@gmail.com>
2019-02-25 22:13:45 +0200

    Update setup.py for publishing on PyPi

--- setup.py
+++ setup.py
@@ -1,11 +1,39 @@
 from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    README = fh.read()
+
 setup(
     name='burnlight',
-    version='0.1.0',
+    version='0.1.3',
     description='GPIO scheduler for Raspberry Pis',
+    long_description=README,
+    long_description_content_type='text/markdown',
     author='Matthew Casserly',
     author_email='',
+    entry_points={
+        'console_scripts': [
+            'burnlight = burnlight.client.client:cli',
+            'burnlightd = burnlight.server.server:main',
+        ]
+    },
+    install_requires = [
+        'flask>=1.0.0',
+        'lark-parser',
+        'gevent',
+        'click',
+        'gpiozero',
+        'requests'
+
+    ],
+    data_files = [
+        ('~/.local/share/systemd/system', ['burnlight/burnlightd.service'])
+    ],
     url='https://github.com/wailashi/burnlight',
-    license=license,
-    packages=find_packages(exclude=('tests'))
+    license='MIT',
+    packages=find_packages(exclude=('tests')),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ]
 )
